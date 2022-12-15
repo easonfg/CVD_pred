61ebb758f221efa82d27122ff981d4373ecd20d3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-15 14:47:17 +0000

    Adding tarfile member sanitization to extractall()

--- composite/functions.py
+++ composite/functions.py
@@ -906,7 +906,26 @@ class RiskScore:
 
     filename = archive_name[:-7]
     with tarfile.open('./{}'.format(archive_name), mode='r') as tar_archive:
-      tar_archive.extractall()
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(tar_archive)
     #os.remove(archive_name)
 
     pkl_filename = './{}.pkl'.format(filename)
